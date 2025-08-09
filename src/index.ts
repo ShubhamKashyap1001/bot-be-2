a2dc0eb2f4c59c06900db37673c284e3c01fbf0c
adarsh singh <adsredmi182615@gmail.com>
2025-08-09 22:24:43 +0530

    dynamic mailing is up

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import express from "express"
-import { PrismaClient } from "../generated/prisma";
 import { testCron } from "./services/cron/cron.service"
 import authRoutes from "./routes/auth.route";
 import dotenv from "dotenv"
 
-const prisma = new PrismaClient();
 
 dotenv.configDotenv()
 const app  = express()
@@ -13,13 +11,6 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 
 //fetch()
-app.get('/',(req,res)=>{
-    
-    res.send("Upp ")
-
-})
-
-
 app.listen(3000,()=>{
     // here i am calling my cron
     testCron()
--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,32 +1,33 @@
 import nodemailer, { createTransport } from "nodemailer"
 import dotenv from "dotenv";
 import { generateMessageHTML } from "../link/codeforces/link.codeforces";
+import { PrismaClient } from "../../../generated/prisma";
+const prisma = new PrismaClient()
 dotenv.config();
 const transporter = createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    }
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  }
 })
-
-
-
 export const sendEmail = async () => {
-  const message = await generateMessageHTML();
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: "shubh9142996613@gmail.com",
-    subject: "Your Daily Codeforces Problems",
-    html: message,
-  };
-
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent:", info.response);
-  } catch (error) {
-    console.error("Error sending email:", error);
+  const user = await prisma.user.findMany({})
+  for (const u of user) {
+    const message = await generateMessageHTML();
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: u.email,
+      subject: "Your Daily Codeforces Problems",
+      html: message,
+    };
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Email sent:", u.email);
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
   }
 };
 
+// like send the mail one by one 
